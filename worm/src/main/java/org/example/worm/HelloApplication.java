37c65366e2ad16b247583ba1cbdf87d306732509
manisv <manich2002@gmail.com>
2024-03-25 16:41:21 +0000

    Food komið

--- worm/src/main/java/org/example/worm/HelloApplication.java
+++ worm/src/main/java/org/example/worm/HelloApplication.java
@@ -3,16 +3,26 @@ package org.example.worm;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import vinnsla.Food;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 600;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
-        stage.setTitle("Hello!");
+        Pane root = fxmlLoader.load();
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+
+        Food food = new Food(WIDTH, HEIGHT);
+        root.getChildren().add(food.getRectangle());
+
+        stage.setTitle("Snake");
         stage.setScene(scene);
         stage.show();
     }

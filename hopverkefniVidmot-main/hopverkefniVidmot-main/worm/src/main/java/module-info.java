d0928b8ee2dc520eb34fbf560c1e6383725f85ce
Ólafur Sær Sigursteinsson <42570202+Amaragance682@users.noreply.github.com>
2024-04-17 13:48:16 +0000

    Delete hopverkefniVidmot-main/hopverkefniVidmot-main directory
    yi, committadi ovart heilu odru repo Lol?

--- hopverkefniVidmot-main/hopverkefniVidmot-main/worm/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.worm {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-
-    opens org.example.worm to javafx.fxml;
-    exports org.example.worm;
-}
